67fcbf942780b53cf43614f7568e3293752a0979
Zhengchen Guan <zgu@objektkultur.de>
2021-06-08 13:34:57 +0200

    Add servicelayer

---
+++ OKBLOGAPI.Service/Service/BookingService.cs
@@ -0,0 +1,13 @@
+﻿using OKBLOGAPI.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OKBLOGAPI.Service.Service
+{
+    public class BookingService : IBookingService
+    {
+    }
+}
---
+++ OKBLOGAPI.Service/Service/TableService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OKBLOGAPI.Service.Interface;
+
+namespace OKBLOGAPI.Service.Service
+{
+    public class TableService : ITableService
+    {
+    }
+}
